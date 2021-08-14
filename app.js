09f43fcf01900eb08025bdf007b9dcb6d780d255
Autumn Raine <autumn@inerdgirl.com>
2021-08-14 09:13:27 -0600

    Finished cycle-1

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const db = require('./db/db');
 const students = require('./routes/students');
 const tests = require('./routes/tests');
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan('dev'));
 
--- db/models/student.js
+++ db/models/student.js
@@ -1,6 +1,27 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student', {
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }, 
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    }
+});
+
+Student.beforeCreate((student) => {
+    student.firstName = student.firstName[0].toUpperCase() + student.firstName.substring(1);
+    student.lastName = student.lastName[0].toUpperCase() + student.lastName.substring(1);
+});
 
 module.exports = Student;
